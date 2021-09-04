26ccc6f5064ca1219d4a602381fb1f961b4d0276
Chani17 <cucute8878@naver.com>
2021-09-04 20:39:38 +0900

    [김찬희/Python/성공/S3]
    [김찬희/Python/성공/S3]

---
+++ 자료구조/10799_kim.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+stick = list(input().rstrip('\n'))
+stack = []
+total = 0
+
+for i in range(len(stick)):
+
+    if stick[i] == ')':
+        stack.pop()
+
+        if stick[i-1] == '(':       # 레이저라면
+            total += len(stack)
+        else:                       # 막대의 끝이라면
+            total += 1
+    else:
+        stack.append(stick[i])
+print(total)
